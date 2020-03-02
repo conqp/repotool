a3984f51a7a231578df7e0f89e67ebad374b4be5
Richard Neumann <r.neumann@homeinfo.de>
2020-03-02 15:10:51 +0100

    Updated version to 1.2.7.

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ from distutils.core import setup
 
 setup(
     name='repotool',
-    version='latest',
+    version='1.2.7',
     author='Richard Neumann',
     author_email='<mail at richard dash neumann period de>',
     maintainer='Richard Neumann',
     maintainer_email='<mail at richard dash neumann period de>',
     py_modules=['repotool'],
     scripts=['repotool'],
-    description=('Arch linux repositry management tool.'))
+    description=('Arch linux repositry management tool.')
+)
