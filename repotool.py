1b6d039171269e7650cb6266fe665fb7bedaf7ad
Richard Neumann <mail@richard-neumann.de>
2021-01-12 17:18:37 +0100

    Refac & added repo mapping.

--- repotool.py
+++ repotool.py
@@ -1,17 +1,19 @@
 """Manage Arch Linux repositories."""
 
-from argparse import ArgumentParser
+from __future__ import annotations
+from argparse import ArgumentParser, Namespace
 from configparser import ConfigParser
 from contextlib import suppress
 from functools import lru_cache
+from json import load
 from logging import DEBUG, INFO, basicConfig, getLogger
 from os import linesep
 from pathlib import Path, PosixPath
-from re import compile  # pylint: disable=W0622
+from re import Match, fullmatch
 from shutil import SameFileError, copy2
 from subprocess import check_call, check_output
 from sys import exit    # pylint: disable=W0622
-from typing import NamedTuple
+from typing import Iterator, NamedTuple, Optional, Tuple, Union
 
 
 __all__ = [
@@ -26,21 +28,23 @@ __all__ = [
 ]
 
 
+CONFIG_FILE = Path('/etc/repotool.conf')
 DESCRIPTION = 'Manage Arch Linux packages and repositories.'
 LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
 LOGGER = getLogger(__file__)
 PACKAGELIST = ('/usr/bin/makepkg', '--packagelist')
 PKG_GLOB = '*.pkg.tar*'
-PKG_REGEX = compile('^.*-(x86_64|i686|any)\\.pkg\\.tar(\\.[a-z]{2,3})?$')
+PKG_REGEX = '^.*-(x86_64|i686|any)\\.pkg\\.tar(\\.[a-z]{2,3})?$'
+REPO_MAP = Path('/etc/repotool.json')
 
 
-def pkgsig(package):
+def pkgsig(package: Path) -> Path:
     """Returns the path to the package's signature."""
 
     return Path(str(package) + '.sig')
 
 
-def signpkg(package):
+def signpkg(package: Path) -> int:
     """Signs the respective pacakge."""
 
     return check_call(
@@ -48,7 +52,7 @@ def signpkg(package):
          str(package)))
 
 
-def pkgpath(pkgdir=None):
+def pkgpath(pkgdir: Union[Path, str, None] = None) -> Iterator[Path]:
     """Yields the paths of the packages to be built."""
 
     text = check_output(PACKAGELIST, text=True, cwd=pkgdir)
@@ -58,24 +62,24 @@ def pkgpath(pkgdir=None):
 
 
 @lru_cache()
-def is_package(string):
+def is_package(string: str) -> Match:
     """Checks whether the path is a package and returns a regex match."""
 
     if not isinstance(string, str):
         return is_package(str(string))
 
-    return PKG_REGEX.fullmatch(string)
+    return fullmatch(PKG_REGEX, string)
 
 
 @lru_cache()
-def vercmp(version, other):
+def vercmp(version: str, other: str) -> int:
     """Compares package versions."""
 
     return int(check_output(('/usr/bin/vercmp', version, other), text=True))
 
 
 @lru_cache()
-def get_pkgbase_and_version(path):
+def get_pkgbase_and_version(path: Path) -> Tuple[str, Version]:
     """Returns the pkgbase and version from the given file path."""
 
     command = ('/usr/bin/pacman', '-Qp', str(path))
@@ -85,20 +89,20 @@ def get_pkgbase_and_version(path):
 
 
 @lru_cache()
-def get_arch_and_compression(path):
+def get_arch_and_compression(path: Path) -> Tuple[str, str]:
     """Returns the architecture and file compression."""
 
     return is_package(path).groups()
 
 
-def get_args():
+def get_args() -> Namespace:
     """Parses and returns the command line arguments."""
 
     parser = ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('repository', help='the target repository')
     parser.add_argument(
         'package', type=Package, nargs='*',
         help='the packages to add to the respository')
+    parser.add_argument('-R', '--repository', help='the target repository')
     parser.add_argument(
         '-c', '--clean', action='store_true',
         help='remove other versions of the package from the repo')
@@ -118,41 +122,71 @@ def get_args():
     return parser.parse_args()
 
 
+def get_repo_map() -> dict:
+    """Returns the target repository."""
+
+    try:
+        with REPO_MAP.open('r') as file:
+            return load(file)
+    except FileNotFoundError:
+        return {}
+
+
+def list_repo(config: ConfigParser, repository: str):
+    """Lists packages in the given repository."""
+
+    repository = Repository.from_config(repository, config[repository])
+
+    for package in repository.packages:
+        print(*package)
+
+
+def add_package(package: Package, config: ConfigParser,
+                args: Namespace) -> bool:
+    """Adds a package to a repository."""
+
+    pkgbase = package.pkgbase
+    repo_map = get_repo_map()
+
+    try:
+        repo_config = config[args.repository or repo_map.get(pkgbase)]
+    except KeyError:
+        LOGGER.error('No repository configured for package: %s', pkgbase)
+        return False
+
+    repository = Repository.from_config(args.repository, repo_config)
+    repository.add(package, sign=args.sign, clean=args.clean)
+
+    if args.rsync:
+        repository.rsync(target=args.target, delete=args.delete)
+
+    return True
+
+
 def main():
     """Main program."""
 
     args = get_args()
     basicConfig(level=DEBUG if args.verbose else INFO, format=LOG_FORMAT)
     config = ConfigParser()
-    config.read('/etc/repotool.conf')
-    repo_config = config[args.repository]
-    repository = Repository.from_config(args.repository, repo_config)
+    config.read(CONFIG_FILE)
 
-    if not args.package:
-        for package in repository.packages:
-            print(*package)
-
+    if args.repository and not args.package:
+        list_repo(config, args.repository)
         exit(0)
 
-    exit_code = 0
+    returncode = 0
 
     for package in args.package:
         try:
-            repository.add(package, sign=args.sign, clean=args.clean)
+            add_package(package, config, args)
         except KeyboardInterrupt:
             print()
-            LOGGER.warning('Skipped adding package %s.', package)
-            exit_code += 1
+            LOGGER.warning('Aborted by user.')
+            returncode += 1
+            continue
 
-    if args.rsync:
-        try:
-            repository.rsync(target=args.target, delete=args.delete)
-        except KeyboardInterrupt:
-            print()
-            LOGGER.warning('Synchronization aborted by user.')
-            exit_code += 1
-
-    exit(exit_code)
+    exit(returncode)
 
 
 class Version(str):
@@ -206,7 +240,7 @@ class Package(PosixPath):
         """Returns the package compression."""
         return get_arch_and_compression(self)[1]
 
-    def is_other_version_of(self, other):
+    def is_other_version_of(self, other: Version) -> bool:
         """Checks if the other package is considered
         another version of this pacakge.
         """
@@ -230,7 +264,7 @@ class Repository(NamedTuple):
     target: str
 
     @classmethod
-    def from_config(cls, name, config):
+    def from_config(cls, name: str, config: ConfigParser) -> Repository:
         """Returns the repository from the given name and configuration."""
         basedir = Path(config['basedir'])
         dbext = config.get('dbext', '.db.tar.zst')
@@ -255,7 +289,7 @@ class Repository(NamedTuple):
         """Yields distinct package names."""
         return {pkg_info.pkgbase for pkg_info in self.packages}
 
-    def _copy_pkg(self, package, sign):
+    def _copy_pkg(self, package: Package, sign: bool) -> None:
         """Copies the package to the repository's base dir."""
         signature = pkgsig(package)
 
@@ -271,13 +305,13 @@ class Repository(NamedTuple):
         with suppress(SameFileError):
             copy2(signature, self.basedir)
 
-    def packages_for_base(self, pkgbase):
+    def packages_for_base(self, pkgbase: str) -> Iterator[Package]:
         """Yields package files with the respective package information."""
         for path in self.basedir.glob(f'{pkgbase}-{PKG_GLOB}'):
             if is_package(path) is not None:
                 yield Package(path)
 
-    def isolate(self, package):
+    def isolate(self, package: Package) -> None:
         """Removes other versions of the given package."""
         for other_package in self.packages_for_base(package.pkgbase):
             if other_package.is_other_version_of(package):
@@ -290,7 +324,8 @@ class Repository(NamedTuple):
             else:
                 LOGGER.debug('Keeping %s.', other_package)
 
-    def add(self, package, *, sign=None, clean=False):
+    def add(self, package: Package, *, sign: bool = False,
+            clean: bool = False) -> None:
         """Adds the respective pacakge to the repo."""
         sign = sign or self.sign
         self._copy_pkg(package, sign)
@@ -305,7 +340,8 @@ class Repository(NamedTuple):
         if clean:
             self.isolate(package)
 
-    def rsync(self, target=None, *, delete=False):
+    def rsync(self, target: Optional[str] = None, *,
+              delete: bool = False) -> int:
         """Synchronizes the repository to the target."""
         target = self.target if target is None else target
 
